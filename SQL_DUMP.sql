c841b225f602da355d1bd89cc396ff3966ab36ce
vscaner <advokatnafan@gmail.com>
2015-03-06 15:07:04 +0300

    Fix name of SQL DUMP ^^

--- SQL_DUMP.sql
+++ SQL_DUMP.sql
@@ -1,12 +1,31 @@
 
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Мар 06 2015 г., 11:58
+-- Версия сервера: 5.1.66
+-- Версия PHP: 5.2.17
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- База данных: `u787682332_vscan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cat`
+--
 
 CREATE TABLE IF NOT EXISTS `cat` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,6 +40,12 @@ CREATE TABLE IF NOT EXISTS `cat` (
   KEY `index` (`index`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cat_list`
+--
+
 CREATE TABLE IF NOT EXISTS `cat_list` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
   `Cat` text COLLATE utf8_unicode_ci NOT NULL,
@@ -29,11 +54,23 @@ CREATE TABLE IF NOT EXISTS `cat_list` (
   KEY `index` (`index`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=75 ;
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `client_platform_type`
+--
+
 CREATE TABLE IF NOT EXISTS `client_platform_type` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
   `name` text COLLATE utf8_unicode_ci NOT NULL,
   KEY `index` (`index`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `comments`
+--
 
 CREATE TABLE IF NOT EXISTS `comments` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
@@ -42,7 +79,13 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`index`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=21 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=32 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `gps`
+--
 
 CREATE TABLE IF NOT EXISTS `gps` (
   `index` int(11) unsigned zerofill NOT NULL AUTO_INCREMENT,
@@ -51,6 +94,12 @@ CREATE TABLE IF NOT EXISTS `gps` (
   `date` int(11) NOT NULL,
   PRIMARY KEY (`index`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `main`
+--
 
 CREATE TABLE IF NOT EXISTS `main` (
   `idmain` int(11) NOT NULL AUTO_INCREMENT,
@@ -65,10 +114,44 @@ CREATE TABLE IF NOT EXISTS `main` (
   `Testinanimals` text CHARACTER SET cp1251 NOT NULL,
   `comment` text,
   `meta` int(30) NOT NULL,
+  `moderate` int(11) DEFAULT '0',
+  `moderator_name` text NOT NULL,
   PRIMARY KEY (`idmain`),
   UNIQUE KEY `barcode` (`barcode`),
   FULLTEXT KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=504 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1195 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `passwords`
+--
+
+CREATE TABLE IF NOT EXISTS `passwords` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `pass` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`index`),
+  UNIQUE KEY `index` (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1005 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `PCClientVErsion`
+--
+
+CREATE TABLE IF NOT EXISTS `PCClientVErsion` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `version` int(11) NOT NULL,
+  PRIMARY KEY (`index`),
+  KEY `index` (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `report_metadata`
+--
 
 CREATE TABLE IF NOT EXISTS `report_metadata` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
@@ -80,7 +163,21 @@ CREATE TABLE IF NOT EXISTS `report_metadata` (
   `report_latitude` double NOT NULL,
   `report_longitude` double NOT NULL,
   KEY `index` (`index`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=35 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=845 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sms`
+--
+
+CREATE TABLE IF NOT EXISTS `sms` (
+  `index` int(11) NOT NULL AUTO_INCREMENT,
+  `SMSSENDER` int(11) NOT NULL,
+  `SMSTEXT` int(11) NOT NULL,
+  PRIMARY KEY (`index`),
+  KEY `index` (`index`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
